bfc54e0b066ef12158e282830c6af28b06e7fc3a
majdibo <majdi.ben.ouaghrem@gmail.com>
2022-11-17 02:29:01 +0100

    add external links

--- src/app/core/models/bio.ts
+++ src/app/core/models/bio.ts
@@ -3,4 +3,11 @@ export interface Bio {
   lastName: string;
   about: string[];
   intro: string[];
+  externalLinks: ExternalLink[];
+}
+
+export interface ExternalLink {
+  link: string;
+  name: string;
+  logo: string;
 }
